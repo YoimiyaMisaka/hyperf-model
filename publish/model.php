3b94a2533cadfd48ae9208c23458f51987a402ed
李灿锴 <13144429534@163.com>
2023-02-17 14:41:27 +0800

    修复问题

--- publish/model.php
+++ publish/model.php
@@ -4,5 +4,6 @@ return [
     'default' => [
         'model_path' => 'app/Infrastructure/Database/Model',
         'column_path' => 'app/Infrastructure/Database/Constant',
+        'database' => 'test'
     ],
 ];
